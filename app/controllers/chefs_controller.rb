e8acf443c441a859bfdad9184d0d5a1d5ec4f9c0
David H <djh@infowest.com>
2014-02-25 12:20:43 -0800

    working for chefs, unchecked for recipe

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -2,10 +2,46 @@ class ChefsController < ApplicationController
 
   def new
     @chef = Chef.new
+  end
 
+  def create
+    @chef = Chef.new(chef_params)
+    if @chef.save
+      flash[:success] = "New chef has been created"
+      redirect_to chef_path(@chef.id)
+    else 
+      flash[:error] = "There was an error saving your form"
+      render :new
+    end
+  end
+
+  def show
+    @chef = Chef.find(params[:id])
+  end
+
+  def edit
+    @chef = Chef.find(params[:id])
+  end
+
+  def update
+    @chef = Chef.find(params[:id])
+    if @chef.update_attributes(chef_params)
+      flash[:notice] = "#{@chef.name} has been updated"
+      redirect_to chef_path(@chef.id)
+    else 
+      flash[:error] = "There was an error updating your form"
+      render :edit
+    end
+  end
+
+  def index
+    @chefs = Chef.all
   end
 
 
 
-
+private
+  def chef_params
+    params.require(:chef).permit(:name, :show, :specialty, :avatar)
+  end
 end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,48 @@
 class RecipesController < ApplicationController
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      flash[:success] = "New recipe has been created"
+      redirect_to recipe_path(@recipe.id)
+    else 
+      flash[:error] = "There was an error saving your form"
+      render :new
+    end
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update_attributes(recipe_params)
+      flash[:notice] = "#{@recipe.name} has been updated"
+      redirect_to recipe_path(@recipe.id)
+    else 
+      flash[:error] = "There was an error updating your form"
+      render :edit
+    end
+  end
+
+  def index
+    @recipe = Recipe.all
+  end
+
+
+
+private
+  def recipe_params
+    params.require(:recipe).permit(:title, :description, :cook_time, :instructions, :picture, :ingredients)
+  end
 end
+
